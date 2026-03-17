d22b006034973d818add1ee9c1a375dad746e1c2
Pedro Henrique <phsilva.net@gmail.com>
2026-03-17 00:16:50 -0300

    feat: sweb-example

---
+++ src/index.tsx
@@ -0,0 +1,4 @@
+import { render } from "sweb/dom";
+
+const root = document.getElementById("root");
+render(() => <main>Batata</main>, root!);
---
+++ vite.config.ts
@@ -0,0 +1,6 @@
+import sweb from "sweb-vite";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  plugins: [sweb()],
+});
